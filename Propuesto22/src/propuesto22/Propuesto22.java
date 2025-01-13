db41d4fb66c1fec258bf7783fb5f6cf91269068f
SamuCasta <118783859+Dagores@users.noreply.github.com>
2025-01-13 00:31:30 -0500

    Act3

---
+++ Propuesto22/src/propuesto22/Propuesto22.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package propuesto22;
+
+/**
+ *
+ * @author david
+ */
+public class Propuesto22 {
+
+    
+     public static void main(String[] args) {
+        Ventana Ventana = new Ventana();
+        Ventana.setVisible(true);
+    }
+    
+}
