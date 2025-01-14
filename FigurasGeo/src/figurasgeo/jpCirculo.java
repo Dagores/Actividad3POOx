244e245152b8fce0cafc9ed56d9e4cbcac51b9b0
SamuCasta <118783859+Dagores@users.noreply.github.com>
2025-01-14 11:05:46 -0500

    Act3

--- FigurasGeo/src/figurasgeo/jpCirculo.java
+++ FigurasGeo/src/figurasgeo/jpCirculo.java
@@ -26,19 +26,130 @@ public class jpCirculo extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jButton2 = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        txtRadioC = new javax.swing.JTextField();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        txtAreaC = new javax.swing.JTextField();
+        txtPerimetroC = new javax.swing.JTextField();
+        btnCalcularC = new javax.swing.JButton();
+        btnLimpiarC = new javax.swing.JButton();
+
+        jButton2.setText("jButton2");
+
+        jLabel1.setText("Radio");
+
+        txtRadioC.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtRadioCActionPerformed(evt);
+            }
+        });
+
+        jLabel2.setText("Area");
+
+        jLabel3.setText("Perimetro");
+
+        btnCalcularC.setText("Calcular");
+        btnCalcularC.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnCalcularCActionPerformed(evt);
+            }
+        });
+
+        btnLimpiarC.setText("Limpiar");
+        btnLimpiarC.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnLimpiarCActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(19, 19, 19)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 77, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(txtRadioC, javax.swing.GroupLayout.PREFERRED_SIZE, 95, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                            .addComponent(btnCalcularC, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, 93, Short.MAX_VALUE)
+                            .addComponent(txtAreaC, javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel2, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 73, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(txtPerimetroC)
+                            .addComponent(btnLimpiarC, javax.swing.GroupLayout.DEFAULT_SIZE, 90, Short.MAX_VALUE))))
+                .addContainerGap(311, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(31, 31, 31)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel1)
+                    .addComponent(txtRadioC, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(40, 40, 40)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel2)
+                    .addComponent(jLabel3))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(txtAreaC, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(txtPerimetroC, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(51, 51, 51)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnCalcularC)
+                    .addComponent(btnLimpiarC))
+                .addContainerGap(210, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
 
+    private void txtRadioCActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtRadioCActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_txtRadioCActionPerformed
+
+    private void btnCalcularCActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCalcularCActionPerformed
+        // TODO add your handling code here:
+        
+      
+        int radio;
+        double area, perimetro;
+        
+        radio = Integer.parseInt(txtRadioC.getText());
+        cCirculo circulo = new cCirculo(radio);
+        area = circulo.calcularArea();
+        perimetro = circulo.calcularPerimetro();
+        
+        txtAreaC.setText(String.valueOf(area));
+        txtPerimetroC.setText(String.valueOf(perimetro));
+        
+    }//GEN-LAST:event_btnCalcularCActionPerformed
+
+    private void btnLimpiarCActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnLimpiarCActionPerformed
+        // TODO add your handling code here:
+        
+        txtRadioC.setText("");
+        txtAreaC.setText("");
+        txtPerimetroC.setText("");
+        
+    }//GEN-LAST:event_btnLimpiarCActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnCalcularC;
+    private javax.swing.JButton btnLimpiarC;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JTextField txtAreaC;
+    private javax.swing.JTextField txtPerimetroC;
+    private javax.swing.JTextField txtRadioC;
     // End of variables declaration//GEN-END:variables
 }
